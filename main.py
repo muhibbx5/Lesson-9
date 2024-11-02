1382fe79c3ce0dc41332368fa79ac4eade275126
muhibbx5 <muhibbali56@gmail.com>
2024-11-02 14:01:30 +0500

    calculations

---
+++ main.py
@@ -0,0 +1,9 @@
+medicalcause=input("Do you have any medical issues, yes or no?")
+attendance=input("enter the attendence of the student?")
+if (medicalcause=='yes'):
+ print("you are eligible to do the exam")
+else:
+ if (attendance>=75):
+  print("You are allowed")
+ else: 
+  ("you are not allowed")
